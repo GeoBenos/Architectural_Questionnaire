a5defa74519b566c6e0c2419bfd41e5c734e7def
George Benos <benosgeorg13@gmail.com>
2023-07-12 16:00:54 +0300

    requirejs and nodemailerjs packages

--- Website/Mailer/mail_app.js
+++ Website/Mailer/mail_app.js
@@ -1,27 +1,29 @@
-const nodemailer = require('nodemailer');
+function sendEmailWithQuestionnaireResults() {
+    console.log(localStorage)
+    require(['nodemailer'], function(nodemailer) {
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "benosgeorg13@gmail.com",
+          pass: "bpvfjwxsgqylmzsw",
+        }
+      });
+  
+      const options = {
+        from: "benosgeorg13@gmail.com",
+        to: "benos.geo@gmail.com",
+        subject: "Questionnaire results",
+        text: JSON.stringify(localStorage),
+      };
+  
+      transporter.sendMail(options, function (err, info){
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("Sent: " + info.response);
+      });
+    });
+  }
 
-console.log()
-
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: "benosgeorg13@gmail.com",
-        pass: "bpvfjwxsgqylmzsw",
-
-    }
-});
-
-const options = {
-    from: "benosgeorg13@gmail.com",
-    to: "benos.geo@gmail.com",
-    subject: "Questionnaire results",
-    text: "TEST TEXT B!ATCH!",
-};
-
-transporter.sendMail(options, function (err, info){
-    if(err){
-        console.log(err);
-        return;
-    }
-    console.log("Sent: " + info.response)
-})
+sendEmailWithQuestionnaireResults();
